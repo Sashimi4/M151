9e617d5f997047040fd6d3450ca0c977e392c8d9
Sashimi4 <sascha.buthelezi@noseryoung.ch>
2022-09-27 17:14:19 +0200

    Implemented functional profile card for user viewing.
    Currently still filled with static data and no dynamic elements. Sizing and actual styling is slightly off. But main structure is achieved.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ProfileShelf from '../shelf-content/ProfileShelf';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
+import ProfileCard from '../components/ProfileCard';
 
 const Home = () => {
 
     return (
-
       /* Interchangeable shelf content */
       <div>
+        <ProfileCard />
+        <hr></hr>
         <ProfileShelf/>
       </div>
     );
